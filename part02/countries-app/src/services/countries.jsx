f2aaaeabca5c03a1f37348a6932e93fb909a2861
Nicolas Cacciavillani <ncacciavillani@gmail.com>
2024-05-12 00:48:43 +0200

    dir cleaning

--- part02/countries-app/src/services/countries.jsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const getAll = () => {
-    const req = axios.get(import.meta.env.VITE_HELSINSKI_API_URL);
-    return req.then(res => res.data)
-}
-
-export default { getAll };
